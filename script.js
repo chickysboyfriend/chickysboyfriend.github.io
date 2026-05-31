fff587499bdf41ec9740dbbdac1c400695d355e6
Cbf <chickysboyfriend@gmail.com>
2026-05-31 15:32:50 +0530

    khana

--- script.js
+++ script.js
@@ -2,20 +2,34 @@ const valentineData = {
     7: { 
         title: "Meri pyaari fool", 
         quote: "(gulab ka)", 
-        img: "res/onam.jpeg",
-        note: "Jaan.. Kaise ho re. (``Theek hu ree``). This is the first photo i think that we've taken ek saath. Pata tha mujhe ye time tak that one day hum dono saath honge, but itna saath, itna pyaar, CRAZY. Itna pyaar mila hain mujhe tujhse, ye kabhi socha nahi tha re life main hoga karke. Very instagram/social media thing to see this happening to me. Dhanyawad aapka cutie. I hope ye aur aur aur badhe, pure chaand tak pohoche.. And sachi kahu toh tu kinda gulab hain re, bohot pyaar and kabhi kabar chubb bhi jaate ho. Chubb jaate, chip jaate, time laga jaan ek ek karke saare kaaton se dosti karne. Tu mere & main tere kaaton se. Aur ab tak ye kaaton se dosti yaari jaari hain. I always always wish, ghanta wish, believe that we will find the softer petal wala side jisse hum ek dusre ko kaaton ko overcome karke, trust karke, in har ek situation. "
-
-},
+        // Handles 2 pictures cleanly
+        img: ["res/onam.jpeg", "res/rose-day.jpeg"],
+        note: "Jaan.. Kaise ho re. (``Theek hu ree``). Onam wala is the first photo i think that we've taken ek saath. Pata tha mujhe ye time tak that one day hum dono saath honge, but itna saath, itna pyaar, CRAZY. Itna pyaar hoga hamare beech, ye kabhi socha nahi tha re life main hoga karke. Very instagram/social media thing to see this happening to me. Dhanyawad aapka cutie. I hope ye aur aur aur badhe, pure chaand tak pohoche.. And sachi kahu toh tu kinda gulab hain re, bohot pyaar and kabhi kabar chubb bhi jaate ho. Chubb jaate, chip jaate, time laga jaan ek ek karke saare kaaton se dosti karne. Tu mere & main tere kaaton se. Aur ab tak ye kaaton se dosti yaari jaari hain. I always always wish, ghanta wish, believe that we will find the softer petal wala side jisse hum ek dusre ko kaaton ko overcome karke, trust karke, in har ek situation."
+    },
     8: { 
         title: "Ye already hoga gaya na ?", 
-        quote: "Ya wapas karna hain ?", img: "res/kfc.jpeg", note: "Tune kal shayad pucha tha (reel se) ki kab aisa laga ki yahi bandi hain full and final karke.. Toh suniyee .. Bohot choti choti baate hain re, ghanto lag jayenge.. Sundarta, khooobsurati, aapki aankhne, smile, NAZARRR, office ke kone konse se padne wali nazar, tere haath, kandhe par til, ungli par til, sampurn aap ye sab hatakar.. As a person, behad behad pyaare, sensitive, caring, emotional, strong, hard working, crazy work ethics, chabuk bandi hain tu. Sab sabse pehle hamari pehlli meet main what stood out is the accident jo hua tha aapke papa ke saath and you being the family anchor, bhai ka bhi covid hua tha, laadh pade the aap hospital main, i'm like guts hain bandi main... I can't tolerate the thought of my parents being sick/ill.. and woh emergency situation ayi nahi kabhi touch wood, but i'm like ladki stud hain. Secondly, woh kanchi cafe main, ambitious wala side.. ki bc kuch life main mast karungi.. dikhaungii meri family ko sabko ki koun hu karke... Phir dheere dheere got to know personal side, tujhe thoda pyaar mile toh tu yaar you'll give 100x in return. Tune jitne aaj tak efforts daale hain humare liye, kabhi mention nahi hua hai woh cheez, and tu kabhi karegi bhi nahi. 1st offsite par, hamara call shuru hua, and baate hote hote we reached to a point ki what exactly you felt for me uss waqt, as you know i'm tragically slow in catching upto speed in these things. Tune kaha, main hu. Bus main hu, jab tak tu new relic main hain. I'm like bus new relic tak ? And phir tune kaha, yaha tak, iske baad, hamesha. I'm like bhaisahab. Chalo shaadi kar lete hain, ho gaya hain mera decision. Subah uthkar maine kafi baari pucha tujhse, madam yaad hain kal raat ka ki daaru pikar bol gaye, ab gand mara bolegi ye. Bohot choti aisi cheeze hain, but jitna bhi likhu, shayad wapas, shayad kya pakka se hi isko edit karunga, but bottom line I felt ki tu hain mere liye, main hu tere liye hamesha, dono ka support rahega hamesha ek dusre ko, hamesha ek dusre ki life behatar karne ka pura try karenge. Ghar set toh baaki muskile aasan hain. Hamesha bohot bohot pyaar chicky." },
+        quote: "Ya wapas karna hain ?", 
+        img: "res/kfc.jpeg", 
+        note: "Tune kal shayad pucha tha (reel se) ki kab aisa laga ki yahi bandi hain full and final karke.. Toh suniyee .. Bohot choti choti baate hain re, ghanto lag jayenge.. Sundarta, khooobsurati, aapki aankhne, smile, NAZARRR, office ke kone konse se padne wali nazar, tere haath, kandhe par til, ungli par til, sampurn aap ye sab hatakar.. As a person, behad behad pyaare, sensitive, caring, emotional, strong, hard working, crazy work ethics, chabuk bandi hain tu. Sab sabse pehle hamari pehlli meet main what stood out is the accident jo hua tha aapke papa ke saath and you being the family anchor, bhai ka bhi covid hua tha, laadh pade the aap hospital main, i'm like guts hain bandi main... I can't tolerate the thought of my parents being sick/ill.. and woh emergency situation ayi nahi kabhi touch wood, but i'm like ladki stud hain. Secondly, woh kanchi cafe main, ambitious wala side.. ki bc kuch life main mast karungi.. dikhaungii meri family ko sabko ki koun hu karke... Phir dheere dide got to know personal side, tujhe thoda pyaar mile toh tu yaar you'll give 100x in return. Tune jitne aaj tak efforts daale hain humare liye, kabhi mention nahi hua hai woh cheez, and tu kabhi karegi bhi nahi. 1st offsite par, hamara call shuru hua, and baate hote hote we reached to a point ki what exactly you felt for me uss waqt, as you know i'm tragically slow in catching upto speed in these things. Tune kaha, main hu. Bus main hu, jab tak tu new relic main hain. I'm like bus new relic tak ? And phir tune kaha, yaha tak, iske baad, hamesha. I'm like bhaisahab. Chalo shaadi kar lete hain, ho gaya hain mera decision. Subah uthkar maine kafi baari pucha tujhse, madam yaad hain kal raat ka ki daaru pikar bol gaye, ab gand mara bolegi ye. Bohot choti aisi cheeze hain, but jitna bhi likhu, shayad wapas, shayad kya pakka se hi isko edit karunga, but bottom line I felt ki tu hain mere liye, main hu tere liye hamesha, dono ka support rahega hamesha ek dusre ko, hamesha ek dusre ki life behatar karne ka pura try karenge. Ghar set toh baaki muskile aasan hain. Hamesha bohot bohot pyaar chicky." 
+    },
     9: { 
-        title: "Jaan", 
-        quote: "Only sweet jispar kabhi diet nahi lagega.", 
-        img: "res/starbucks.jpeg",
-        note: "Can't think of anything abhi, apart from aaj ghar aaye aap, good cozy time bus khana nahi khilaya aur ghar nahi chora maine. Feeling shitty. Agli baar se kuch bhi ho, ye dono cheeze hongi, baaki ho na ho." },
-    10: { title: "Teddy Day", quote: "Safe space.", img: "res/traffic.jpeg", note: "Ye dekh, itne saare traffic main apna romance chal raha tha yaha. Waise hi tere ya mere life main kaise bhi aur kitni bhi ho chutiyape, aag hi lagi padi ho, hamare hone par sab shaanth hain baki ka chaos. Bed side teddy jaise hamesha hug karke soyenge bc. Jaise tune uss raat jo bhi jaisi bhi halat main thi tu, uthaya, cuddle kiya, core core moment jaan life ka. Kabhi nahi bhulunga. Saala tu hi mera teddy hain mast. Love you bohot bohot kum hain. Hamesha - Kaustubh." },
-    11: { title: "Promise Day", quote: "Promise, ki", img: "res/diwalivisit.jpeg", note: "Jaan, hello from Blr. Very simple yaar, promise to always be there for you, love, respect, & adore you. Hameshaa. Make myself better each and every day for you, for us. Har chiz jo teri problem hain meri problem hain, har jeet teri yaar i'll be crazy crazy happy. Har baat sunna, samajna, har baat sort out kar lenge, bohot shaanti se. Mast ghar banayenge, careers banging, body banging, paise khoob, trips and kapde sexy. Bus ek promise, dono ek dusre par kabhi kabhi give up nahi karenge, and no one enters our space, in any sense jaan, hamara space, baate sab private. Hamesha hamesha wala pyaar - Jaan " },
+        title: "Khanaaa", 
+        quote: "Jeete hain khane ke liye", 
+        img: ["res/cafe-pizza.jpeg","res/chocolate.jpeg"],
+        note: "Khate peete rahenge ekdum mast jaanuu, Khana aur ek dusre ko bhi.." 
+    },
+    10: { 
+        title: "Teddy Day", 
+        quote: "Safe space.", 
+        img: "res/traffic.jpeg", 
+        note: "Ye dekh, itne saare traffic main apna romance chal raha tha yaha. Waise hi tere ya meer life main kaise bhi aur kitni bhi ho chutiyape, aag hi lagi padi ho, hamare hone par sab shaanth hain baki ka chaos. Bed side teddy jaise hamesha hug karke soyenge bc. Jaise tune uss raat jo bhi jaisi bhi halat main thi tu, uthaya, cuddle kiya, core core moment jaan life ka. Kabhi nahi bhulunga. Saala tu hi mera teddy hain mast. Love you bohot bohot kum hain. Hamesha - Kaustubh." 
+    },
+    11: { 
+        title: "Promise Day", 
+        quote: "Promise, ki", 
+        img: "res/diwalivisit.jpeg", 
+        note: "Jaan, hello from Blr. Very simple yaar, promise to always be there for you, love, respect, & adore you. Hameshaa. Make myself better each and every day for you, for us. Har chiz jo teri problem hain meri problem hain, har jeet teri yaar i'll be crazy crazy happy. Har baat sunna, samajna, har baat sort out kar lenge, bohot shaanti se. Mast ghar banayenge, careers banging, body banging, paise khoob, trips and kapde sexy. Bus ek promise, dono ek dusre par kabhi kabhi give up nahi karenge, and no one enters our space, in any sense jaan, hamara space, baate sab private. Hamesha hamesha wala pyaar - Jaan" 
+    },
     12: { title: "Hug Day", quote: "The best place in the world is inside a hug.", img: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHpueGZid3R6bm9oZzR6bm9oZzR6bm9oZzR6bm9oZzR6bm9oZzR6JmVwPXYxX2ludGVybmFsX2dpZl9ieV9pZCZjdD1n/u9B3S2Arf_COQ/giphy.gif", note: "Imagine I'm hugging you right now." },
     13: { title: "Kiss Day", quote: "A kiss is a lovely trick designed by nature.", img: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHpueGZid3R6bm9oZzR6bm9oZzR6bm9oZzR6bm9oZzR6bm9oZzR6JmVwPXYxX2ludGVybmFsX2dpZl9ieV9pZCZjdD1n/lTQF061vwEBGg/giphy.gif", note: "Sending you a thousand virtual kisses." },
     14: { title: "Valentine's Day", quote: "You are my heart, my soul, my everything.", img: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHpueGZid3R6bm9oZzR6bm9oZzR6bm9oZzR6bm9oZzR6bm9oZzR6JmVwPXYxX2ludGVybmFsX2dpZl9ieV9pZCZjdD1n/26BRv0ThflsHCqfJA/giphy.gif", note: "Happy Valentine's Day! Thank you for making my life beautiful." }
@@ -26,8 +40,7 @@ function moveButton() {
     const noBtn = document.getElementById('noBtn');
     const card = document.getElementById('mainCard');
     
-    // Boundary calculations relative to the card
-    const padding = 20;
+    const padding = 30;
     const maxX = card.clientWidth - noBtn.offsetWidth - padding;
     const maxY = card.clientHeight - noBtn.offsetHeight - padding;
 
@@ -57,6 +70,29 @@ function showDay(num) {
     
     document.getElementById('display-date').innerText = `Feb ${num}: ${day.title}`;
     document.getElementById('quote').innerText = `"${day.quote}"`;
-    document.getElementById('main-image').src = day.img;
     document.getElementById('day-note-text').innerText = day.note;
+
+    const mediaContainer = document.getElementById('media-container');
+    mediaContainer.innerHTML = ''; // Empty the layout to rebuild safely
+
+    // Check if day.img is an array of multiple images or a single path string
+    if (Array.isArray(day.img)) {
+        day.img.forEach(imgSrc => {
+            const imgElement = document.createElement('img');
+            imgElement.src = imgSrc;
+            imgElement.className = "custom-day-img";
+            imgElement.onerror = function() {
+                this.src = 'https://img.icons8.com/emoji/96/000000/heart-suit.png';
+            };
+            mediaContainer.appendChild(imgElement);
+        });
+    } else {
+        const imgElement = document.createElement('img');
+        imgElement.src = day.img;
+        imgElement.className = "custom-day-img";
+        imgElement.onerror = function() {
+            this.src = 'https://img.icons8.com/emoji/96/000000/heart-suit.png';
+        };
+        mediaContainer.appendChild(imgElement);
+    }
 }
